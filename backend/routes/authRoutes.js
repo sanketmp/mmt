0e7e5ebe5ee67cf8297690485e215975205fef50
sanketmp <sanketmpol@gmail.com>
2024-07-07 18:25:13 +0530

    test

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logoutuser, register } from "../controllers/auth.js";
+import { login, logoutuser, register } from "../controllers/authcontroller.js";
 
 const router = express.Router();
 
