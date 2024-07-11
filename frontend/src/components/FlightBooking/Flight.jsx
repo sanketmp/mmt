c57b686098611534ca5912b5ae1579be529ff3d8
sanketmp <sanketmpol@gmail.com>
2024-07-11 23:52:58 +0530

    Create Flight form

---
+++ frontend/src/components/FlightBooking/Flight.jsx
@@ -0,0 +1,122 @@
+import { Link } from "react-router-dom";
+import "./Flight.css";
+import { useState, useEffect } from "react";
+
+const Flight = () => {
+  const [airports, setAirports] = useState([]);
+  const [departure, setDeparture] = useState("");
+  const [returnDate, setReturnDate] = useState("");
+  const [travellers, setTravellers] = useState();
+  const [classType, setClassType] = useState("Economy/Premium Economy");
+
+  //   useEffect(() => {
+  //     // Fetch the JSON data
+  //     fetch("/airports.json")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setAirports(data);
+
+  //         // Set a random default value
+  //         const randomAirport = data[Math.floor(Math.random() * data.length)];
+  //         setSelectedAirport(randomAirport.code);
+  //       })
+  //       .catch((error) => console.error("Error fetching airport data:", error));
+  //   }, []);
+
+  return (
+    <>
+      <div className="search-container">
+        <div className="search-box">
+          <div className="search-box-content">
+            <div className="search-form">
+              <div className="search-form-group">
+                <label className="search-form-label">From</label>
+                <select
+                  className="search-form-select"
+                  onChange={(e) => setSelectedAirport(e.target.value)}
+                >
+                  {/* <option key="{u}" value="">
+                    wtfWTF
+                  </option> */}
+                  {airports.map((airport) => (
+                    <option key={airport.code} value={airport.code}>
+                      {airport.name} ({airport.code})
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="search-form-group-center">
+                <div className="search-form-icon">⇄</div>
+              </div>
+              <div className="search-form-group">
+                <label className="search-form-label">To</label>
+                <select
+                  className="search-form-select"
+                  onChange={(e) => setSelectedAirport(e.target.value)}
+                >
+                  {airports.map((airport) => (
+                    <option key={airport.code} value={airport.code}>
+                      {airport.name} ({airport.code})
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="search-form-group">
+                <label className="search-form-label">Departure</label>
+                <input
+                  type="date"
+                  className="search-form-input"
+                  value={departure}
+                  onChange={(e) => setDeparture(e.target.value)}
+                />
+                <div className="search-form-date-info">
+                  {new Date(departure).toLocaleDateString("en-IN", {
+                    weekday: "long",
+                  })}
+                </div>
+              </div>
+              <div className="search-form-group">
+                <label className="search-form-label">Return</label>
+                <input
+                  type="date"
+                  className="search-form-input"
+                  value={returnDate}
+                  onChange={(e) => setReturnDate(e.target.value)}
+                />
+              </div>
+              <div className="search-form-group">
+                <label className="search-form-label">Travellers & Class</label>
+                <div className="search-form-travellers-class">
+                  <input
+                    type="number"
+                    className="search-form-travellers-input"
+                    value={travellers}
+                    onChange={(e) => setTravellers(e.target.value)}
+                    min="1"
+                  />
+                  <select
+                    className="search-form-class-select"
+                    value={classType}
+                    onChange={(e) => setClassType(e.target.value)}
+                  >
+                    <option>Economy</option>
+                    <option>Premium Economy</option>
+                    <option>Business</option>
+                    <option>First Class</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="search-button-container">
+        <Link to={"/flight-list"}>
+          <button className="search-button">Search Flight</button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Flight;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Featured from "../../components/featured";
 import FeaturedProperties from "../../components/featuredprops";
+import Flight from "../../components/FlightBooking/Flight";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import MailList from "../../components/mail";
@@ -10,16 +11,16 @@ import "./home.css";
 const Home = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <Header />
       <div className="homeContainer">
-        <Featured />
+        {/* <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <Properties />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
-        <Footer />
+        <Footer /> */}
+        <Flight />
       </div>
     </div>
   );
